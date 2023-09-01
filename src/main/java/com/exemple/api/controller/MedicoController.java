45186534af10afd07ab4744e5fbf2b39e4a19ee6
Eduardo <ceduardolima2000@gmail.com>
2023-09-01 16:47:58 -0300

    Fazedo listagem a listagem de médicos usando paginação

--- src/main/java/com/exemple/api/controller/MedicoController.java
+++ src/main/java/com/exemple/api/controller/MedicoController.java
@@ -1,15 +1,19 @@
 package com.exemple.api.controller;
 
 import com.exemple.api.doctor.DadosCadastroMedico;
+import com.exemple.api.doctor.DadosListagemMedico;
 import com.exemple.api.doctor.Medico;
 import com.exemple.api.doctor.MedicoRepository;
 import jakarta.validation.Valid;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -22,5 +26,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico doctorRegisterData) {
         repository.save(new Medico(doctorRegisterData));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
