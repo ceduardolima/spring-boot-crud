dd1e4fc62932301442342127dae172d8fa49b8be
Eduardo <ceduardolima2000@gmail.com>
2023-09-03 13:57:01 -0300

    Retornando token de autenticação

--- src/main/java/com/exemple/api/domain/controller/AutenticacaoController.java
+++ src/main/java/com/exemple/api/domain/controller/AutenticacaoController.java
@@ -1,6 +1,8 @@
 package com.exemple.api.domain.controller;
 
 import com.exemple.api.domain.usuario.DadosAutenticacao;
+import com.exemple.api.domain.usuario.Usuario;
+import com.exemple.api.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +20,15 @@ public class AutenticacaoController {
 
     @Autowired
     private AuthenticationManager manager;
+    @Autowired
+    private TokenService tokenService;
 
     @PostMapping
     @Transactional
     public ResponseEntity login(@RequestBody @Valid DadosAutenticacao dados) {
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(token);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
     }
 
 }
